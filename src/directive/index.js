c903e4786a2270f9e95f480bba98bd342554e27f
wwm <wangwm@szhcf.com.cn>
2021-04-02 18:42:25 +0800

    自定义指令

--- src/directive/index.js
+++ src/directive/index.js
@@ -1,14 +1,12 @@
 import copy from "./copy";
 import longpress from "./longpress";
 import debounce from "./debounce";
-import LazyLoad from "./LazyLoad";
 import waterMarker from "./waterMarker";
 // 自定义指令
 const directives = {
   copy,
   longpress,
   debounce,
-  LazyLoad,
   waterMarker
 };
 
